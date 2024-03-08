6c96db5c7d4049f245fbc7c9dde98940c2bea2e6
Felipe Cristancho <felipecristancho10@gmail.com>
2024-03-08 18:25:57 -0500

    Se crea el esqueleto inicial para la estructura de la arquitectura hexagonal

---
+++ src/main/java/com/felipe/HotelApplication.java
@@ -0,0 +1,16 @@
+package com.felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan({"com.felipe"})
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
+
+}
